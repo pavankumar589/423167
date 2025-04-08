1c79bd05c9a47e4cce610536545037e3d1606a48
pavankumar589 <pavankumarpeethani5@gmail.com>
2025-04-08 15:07:11 +0530

    08/04/2025

---
+++ LAB1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Q 1: Create a directory
+mkdir main_directory
+
+# Q 2: Create a subdirectory inside the directory created
+mkdir main_directory/sub_directory
+
+# Q3: Change to the subdirectory
+cd main_directory/sub_directory
+
+# Q4: Display the calendar for the current month
+cal
+
+# Q5: Get a directory listing of the parent directory
+echo "Listing of parent directory:"
+ls ..
+
